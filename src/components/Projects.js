09f2f9848201613a6bb12a4e17d6951932ff83ef
anonymous3434 <asishsankhyan1834@gmail.com>
2025-12-12 16:56:07 +0530

    Initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,89 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import social from "../assets/img/social.webp";
+import roadMap from "../assets/img/roadMap.webp";
+import weather from "../assets/img/weather.webp";
+import snapgram from "../assets/img/snapgram.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Snapgram",
+      description: "Social Media App",
+      imgUrl: social,
+    },
+    {
+      title: "Weather App",
+      description: "Smart Weather App",
+      imgUrl: weather,
+    },
+    {
+      title: "RoadMap Generator",
+      description: "Generates raodmap for any tasks",
+      imgUrl: roadMap,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Here are some of the projects I’ve built to strengthen my
+                    skills in React, Redux, TypeScript, and modern web
+                    development. Each project reflects my focus on clean UI,
+                    performance, and building real-world, scalable applications.
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
