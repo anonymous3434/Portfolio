bb1c43c00db3dd60fdc284d8d70c19715582a866
anonymous3434 <asishsankhyan1834@gmail.com>
2025-12-12 18:57:14 +0530

    added now projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,8 @@ import { ProjectCard } from "./ProjectCard";
 import social from "../assets/img/social.webp";
 import roadMap from "../assets/img/roadMap.webp";
 import weather from "../assets/img/weather.webp";
-import snapgram from "../assets/img/snapgram.png";
+import pizza from "../assets/img/pizza.png";
+import task from "../assets/img/task.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -14,18 +15,42 @@ export const Projects = () => {
       title: "Snapgram",
       description: "Social Media App",
       imgUrl: social,
+      type: "dynamic",
+      projectLink:
+        "https://social-media-app-git-main-asishs-projects-633477a3.vercel.app?_vercel_share=Xjn0D7KW4HIIPbD4fAGPrnJtdEPd5rQC",
     },
     {
       title: "Weather App",
       description: "Smart Weather App",
       imgUrl: weather,
+      type: "dynamic",
+      projectLink:
+        "https://weather-app-git-master-asishs-projects-633477a3.vercel.app?_vercel_share=RyQr2bvds59lV8e4opsT7lgQfAHGfzP6",
     },
     {
       title: "RoadMap Generator",
       description: "Generates raodmap for any tasks",
       imgUrl: roadMap,
+      type: "dynamic",
+      projectLink: "#",
+    },
+    {
+      title: "Pizza Parlor",
+      description: "Pizza ordering app",
+      imgUrl: pizza,
+      type: "static",
+      projectLink: "https://pizza-parlorzz.netlify.app/",
+    },
+    {
+      title: "Task Manager",
+      description: "Task Managing app",
+      imgUrl: task,
+      type: "static",
+      projectLink: "https://karamsuchi.netlify.app/",
     },
   ];
+  const dynamicProjects = projects.filter((p) => p.type === "dynamic");
+  const staticProjects = projects.filter((p) => p.type === "static");
 
   return (
     <section className="project" id="projects">
@@ -53,10 +78,10 @@ export const Projects = () => {
                       id="pills-tab"
                     >
                       <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        <Nav.Link eventKey="first">Dynamic Apps</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        <Nav.Link eventKey="second">Static Apps</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
                         <Nav.Link eventKey="third">Tab 3</Nav.Link>
@@ -68,12 +93,29 @@ export const Projects = () => {
                         isVisible ? "animate__animated animate__slideInUp" : ""
                       }
                     >
+                      {/* Dynamic Apps */}
                       <Tab.Pane eventKey="first">
-                        <Row>
-                          {projects.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
-                          })}
+                        <Row className="g-4">
+                          {dynamicProjects.map((project, index) => (
+                            <ProjectCard key={index} {...project} />
+                          ))}
                         </Row>
+                      </Tab.Pane>
+
+                      {/* Static Apps */}
+                      <Tab.Pane eventKey="second">
+                        <Row className="g-4">
+                          {staticProjects.map((project, index) => (
+                            <ProjectCard key={index} {...project} />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+
+                      {/* Third Tab Placeholder */}
+                      <Tab.Pane eventKey="third">
+                        <h4 className="text-center">
+                          More projects coming soon...
+                        </h4>
                       </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
